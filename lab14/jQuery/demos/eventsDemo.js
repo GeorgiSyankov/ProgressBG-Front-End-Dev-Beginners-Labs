342dd18c3bec6786bf20aedbe520100cdbb57494
WWWCourses <ivawebcourses@gmail.com>
2020-04-05 17:05:34 +0300

    Lab14: 05.04.2020 DONE: Bring Semantic to the Web NEXT: devTools; Preprocessors

---
+++ lab14/jQuery/demos/eventsDemo.js
@@ -0,0 +1,18 @@
+$(function () {
+	//hide all descriptions:
+	// $('.description').hide();
+
+	// $('.item').on('click', '.title', function (e) {
+	// 	$(this).css('color','red');
+	// })
+
+
+	// // show description on title click:
+	// $('.title').click(function (e) {
+	// 	// compare 'e' vs 'e.target'
+	// 	// compare 'this' vs '$(this)
+	// 	console.dir($(this).children());
+	// 	$(this).parent().children('.description').toggle(800);
+
+	// })
+})
