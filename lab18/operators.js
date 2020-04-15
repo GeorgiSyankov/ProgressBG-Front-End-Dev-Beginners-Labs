42a5b8f4295595a49c513da6e44eaa346c3646b7
WWWCourses <ivawebcourses@gmail.com>
2020-04-15 22:40:04 +0300

    Lab18: 15.04.2020 DONE: till http://wwwcourses.github.io/ProgressBG-Front-End-Dev/pages/themes/beginners/JS_BasicSyntax_1/JS_BasicSyntax_1.html#/11/11 NEXT: next

---
+++ lab18/operators.js
@@ -0,0 +1,73 @@
+// let number1 = 5;
+// let fraction = 3.14;
+
+
+// let userName = "Ada is the\tfirst \"programmer\" in \a the \\wrold\\.";
+// console.log(userName);
+
+// let poem = "line1\nline1\nline1";
+// console.log(poem);
+
+// let newPoem = `
+// line1
+// line2l
+// line3
+// `;
+
+// let x = 5;
+// console.log("x");
+// console.log(x);
+
+
+// let simpleString = "2+4";
+// let templateString = `templateString = ${2+4}`;
+// console.log(simpleString);
+// console.log(templateString);
+
+
+// console.log( true );
+// console.log( "true" );
+// console.log( 5 + 5 );
+//
+
+
+// let userAge = "24";
+// console.log( typeof( undefined ) );
+
+
+
+// let x = undefined;
+// console.log( x );
+
+// let y;
+// console.log( y );
+
+
+// var x;
+// console.log( typeof NaN );
+
+// let i = 1;
+// let resSufix = i++; // i++ == i
+// console.log(`resSufix = ${resSufix}`);
+// console.log( `i = ${i}` ); // i = 2
+
+// let j = 1;
+// let resPrefix = ++j;// ++j == j+1
+// console.log(`resPrefix = ${resPrefix}`);
+// console.log(`j = ${j}`);
+
+// let a = 1, b = 1;
+// let res = a++ + ++b;
+// console.log(`res = ${res}`); // 3
+// console.log(`a = ${a}, b=${b}`); // 1,2 | 2,2
+
+// console.log( Math.pow(2,3) );
+
+let i = 1;
+console.log(2 + (i+=1) );
+
+
+
+
+
+
---
+++ lab18/variables.js
@@ -0,0 +1,25 @@
+// TASK: Намерете лицето на правоъгълник със страни 6 см и 4 см.
+// Резултата запишете в променлива и изпишете стойността й в козолата
+
+let sideA=6;
+let sideB=4;
+let result                          = sideA*sideB;
+console.log(result);
+
+
+
+let a = 5;
+let b = 4;
+let res = (a*b);
+console.log(res);
+
+
+let width = 6;
+let height = 4;
+let area = width * height;
+console.log( area);
+
+
+ let lengh_t = 6;
+ let width = 4;
+ let area = (lengh_t * width)
