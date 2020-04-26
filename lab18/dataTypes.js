efc8341777511cf948328a51aead7bf65c84eb3e
WWWCourses <ivawebcourses@gmail.com>
2020-04-26 13:32:24 +0300

    Lab18: 26.04.2020 DONE: till http://wwwcourses.github.io/ProgressBG-Front-End-Dev/pages/themes/beginners/JS_BasicSyntax_1/JS_BasicSyntax_1.html#/11/3 NEXT: next

---
+++ lab18/dataTypes.js
@@ -0,0 +1,100 @@
+/* -------------------------------------------------------------------------- */
+/*                                   numbers                                  */
+/* -------------------------------------------------------------------------- */
+// let a;
+// let b;
+
+
+// let a = 5;
+// let b = 8;
+
+// let a=5.54, b = 8 ;
+// console.log( a, b );
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                   string                                   */
+/* -------------------------------------------------------------------------- */
+// let userName = 'Ada';
+// console.log( userName );
+
+// let x = 5; 			// 00000101
+// let y = '5';        // 00000110
+
+// let str = '|n|\n|';
+// console.log( str );
+
+// old way
+// let poem = "line 1\nline2\nline3";
+
+// the new way
+// let poem = `line 1
+// line 2
+// line 3
+// `;
+// console.log( poem );
+
+// let str = `${2+3}`;
+
+
+// variable interpolation:
+// let userName = 'Ada';
+// let userLastName = 'Byron';
+
+// let greetMsg = 'Hello, ' + userName + ' ' + userLastName + '!';
+// let greetMsg = `Hello, ${userName} ${userLastName}!`;
+// console.log( greetMsg );
+// Hello, Ada Byron!
+
+
+/* -------------------------------------------------------------------------- */
+/*                                boolean type                                */
+/* -------------------------------------------------------------------------- */
+// let x = 5;
+
+// let true ='error';
+// let 5 = 'error';
+// let 'var' = 'error'
+
+// let flag = true;
+// let str = 'true';
+
+// let x = 5;
+// let y = '5';
+
+// console.log( flag );
+
+// let obj = null;
+
+// console.log( typeof(null) );
+
+
+// let userBirthYear = undefined;
+// let userBirthYearInput = document.querySelector(`input[name='userBirthYear']`);
+// userBirthYearInput.addEventListener('change', function () {
+
+// 	let userBirthYear = userBirthYearInput.value;
+// 	console.log(`userBirthYear = ${userBirthYear}`);
+
+// 	let userAge = 2020 - userBirthYear;
+
+// 	console.log(`userAge: ${userAge}`);
+
+// 	// console.log( typeof(userAge) );
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
