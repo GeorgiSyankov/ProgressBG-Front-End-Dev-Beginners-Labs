acc7f95956d8b89e00285fdf10cdde088d5e2a76
WWWCourses <ivawebcourses@gmail.com>
2020-05-04 22:14:43 +0300

    AoA: Fruits Demo

--- lab23/onArrays/AoADemoOnTask.js
+++ lab23/onArrays/AoADemoOnTask.js
@@ -3,8 +3,9 @@ var fruitsData = [
 	[1.2, 2.0, 3.5]
 ];
 
-for (let i = 0; i < fruitsData.length; i++) {
-
-
-
+for (let  j = 0; j < fruitsData[0].length; j++) {
+	for (let i = 0; i < fruitsData.length; i++) {
+		console.log(fruitsData[i][j]);
+	}
+	console.log(`~~~`);
 }
