4c93a091a9836a4932986002872309c1c2f50ae7
WWWCourses <ivawebcourses@gmail.com>
2020-07-13 13:35:25 +0300

    updates: lab3: more code comments

--- lab3/js/showFile.js
+++ lab3/js/showFile.js
@@ -6,9 +6,19 @@ function showDetailsOld(event) {
 }
 
 function showDetailsNew(orderNumber) {
-	return function () {
-		console.dir(orderNumber);
+	return function (event) {
+		// to share a value between pages we can use:
+		// 1. global variable (bad practice);
+		// 2. add it in URL query string (too complicated, and mainly used for client-server communication)
+		// 3. cookies (mainly used for client-server communication)
+		// 4. local storage/session storage (best for new browsers: https://caniuse.com/#search=local%20storage)
+
 		localStorage.setItem("orderNumber", orderNumber);
+
+		// demo with window.open:
+		if(event.target.tagName === 'BUTTON'){
+			window.open('pages/details.html', "_blank");
+		}
 	}
 }
 
@@ -18,6 +28,8 @@ let rows = document.querySelectorAll('#orders tr');
 
 for (let i = 0; i < rows.length; i++) {
 	const row = rows[i];
+
+	// get orderNumber from <td id="orderNumber">3307</td>
 	let orderNumber = row.querySelector('#orderNumber').innerHTML;
 
 	let btn = row.querySelector('button');
